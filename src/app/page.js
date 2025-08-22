e5ba5af4cee4c5febe236be947ccea4071d9ad57
SaymaShipra <saymaaktershipra@gmaile.com>
2025-08-22 17:19:00 +0600

    added navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1 className="text-amber-900">hello world</h1>;
+  return <h1 className="text-amber-900 kbd kbd-m">hello world</h1>;
 }
